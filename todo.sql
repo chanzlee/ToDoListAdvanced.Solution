a074fe1b09274bfe3dc9fd924e58dbd5e51f6803
Chan Lee <chanethanlee@gmail.com>
2018-09-25 00:09:43 -0700

    add database sql file.

--- todo.sql
+++ todo.sql
@@ -3,18 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 24, 2018 at 05:08 AM
+-- Generation Time: Sep 25, 2018 at 07:08 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `todo`
@@ -38,10 +32,7 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'Workout'),
-(2, 'Clean'),
-(3, 'Eating'),
-(4, 'Study');
+(1, 'Epicodus');
 
 -- --------------------------------------------------------
 
@@ -50,10 +41,17 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `categories_items` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `category_id` int(11) DEFAULT NULL,
-  `item_id` int(11) DEFAULT NULL
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories_items`
+--
+
+INSERT INTO `categories_items` (`id`, `category_id`, `item_id`) VALUES
+(1, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -64,20 +62,18 @@ CREATE TABLE `categories_items` (
 CREATE TABLE `items` (
   `id` int(11) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `duedate` varchar(255) NOT NULL
+  `duedate` datetime NOT NULL,
+  `done` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `description`, `duedate`) VALUES
-(1, 'do dishes', '2018-09-16 00:00:00'),
-(2, 'cook', '2018-09-05'),
-(3, 'cook', '2018-09-05'),
-(4, 'cook', '2018-09-05'),
-(5, 'run', '2018-09-25'),
-(6, 'Read Epicodus material', '2018-09-26 00:00:00.000000');
+INSERT INTO `items` (`id`, `description`, `duedate`, `done`) VALUES
+(3, 'copy papers', '2018-09-04 00:00:00', 0),
+(4, 'make dictionary', '2018-09-12 00:00:00', 0),
+(5, 'make notebook', '2018-09-12 00:00:00', 0);
 
 --
 -- Indexes for dumped tables
@@ -93,8 +89,7 @@ ALTER TABLE `categories`
 -- Indexes for table `categories_items`
 --
 ALTER TABLE `categories_items`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `items`
@@ -110,20 +105,16 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `categories_items`
 --
 ALTER TABLE `categories_items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
